dcf62d5553de853ec8132c5822397b93262736d8
krishnasabhani2007-cloud <krishnasabhani2007@gmail.com>
2026-01-26 19:06:48 +0530

    -a

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 
 from flask import Flask, render_template, jsonify, request
-#from openai import OpenAI
+from openai import OpenAI
 
 
 app = Flask(__name__)
-#client = OpenAI(api_key="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")
+client = OpenAI(api_key="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")
 
 @app.route("/")
 def home():
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,5 +4,7 @@ module.exports = {
   theme: {
     extend: {},
   },
+  apiKey: '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',
+  apiUrl: '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',
   plugins: [],
 }
